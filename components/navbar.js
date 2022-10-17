469ff917619cd94299b4bbd73d28f1fcbdb35b9b
Sebastián <69972223+seba-campo@users.noreply.github.com>
2022-10-17 14:47:34 -0300

    hecho el menu modal

--- components/navbar.js
+++ components/navbar.js
@@ -27,11 +27,48 @@ function navbarComponent(target) {
                 </div>
             </div>
         </div>
-    
+
+        <div class="navbar__menu-modal">
+                <div class="menu__close-btn">
+                    <h2 class="">X</h2>
+                </div>
+                <div class="navbar__menu-modal-el">
+                    <a href="#">HOME</a>
+                </div>
+                <div class="navbar__menu-modal-el">
+                    <a href="#">CONTACTO</a>
+                </div>
+                <div class="navbar__menu-modal-el">
+                    <a href="#">EXPERIENCIAS</a>
+                </div>
+                <div class="navbar__menu-modal-el">
+                    <a href="#">SOBRE MI</a>
+                </div>
+         </div>
+
+
         <div class="header__home">
             <h1 class="header__bienvenida">Hola <br> soy Seba</h1>
         </div>
     </div>
     `
     target.appendChild(containerEl);
-}
+
+    const hamburguer = document.querySelector('.navbar__hamburger');
+    const modalMenu  = document.querySelector('.navbar__menu-modal');
+
+    hamburguer.addEventListener('click', ()=>{
+        modalMenu.setAttribute('style', 'display: flex');
+        console.log("clicked hamb")
+    })
+
+    const closeButton = document.querySelector('.menu__close-btn');
+    closeButton.addEventListener('click', ()=>{
+        modalMenu.setAttribute('style', 'display: none');
+    })
+
+};
+
+// function listenerHamburguer(){
+
+// }
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,13 @@ function main() {
     const footerSection = document.querySelector(".footer");
     const contactSection = document.querySelector(".contact");
     const headerSection = document.querySelector(".header");
+    
     footerComponent(footerSection)    
+    
     contactComponent(contactSection)
+    
     navbarComponent(headerSection)
+    // listenerHamburguer();
 }
 
 main();
